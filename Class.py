d9015f98f722126abdfc9148ed0d083d3aaf36e1
Truhman-san <p.lehr@mail.de>
2021-04-26 17:23:55 +0200

    Funktion solve hinzugefügt

---
+++ Class.py
@@ -0,0 +1,17 @@
+import pprint
+
+def solve(bo):
+    find = find_empty(bo)
+    if find:
+        row, col = find
+    else:
+        return True
+
+    for i in range(1,10):
+        if valid(bo, (row, col), i)
+            bo[row][col] = 1
+            if solve(bo):
+                return True
+            
+            bo[row][col] = 0
+    return False
